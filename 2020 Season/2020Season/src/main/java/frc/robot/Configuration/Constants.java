0d6a33971482e97df3bb310484f0906f82ce35b4
Colin Ross <crosstcp@gmail.com>
2020-01-11 15:21:32 -0500

    Commented unused constants.

--- 2020 Season/2020Season/src/main/java/frc/robot/Configuration/Constants.java
+++ 2020 Season/2020Season/src/main/java/frc/robot/Configuration/Constants.java
@@ -3,12 +3,13 @@ package frc.robot.Configuration;
 public final class Constants {
 
     //Test Robot Settings
+    /*
     public static final int kLeftDriveId = 0;
     public static final int kRightDriveId = 1;
 
     public static final int kDriverUsbSlot = 0;
     public static final int kOperatorUsbSlot = 1;
-
+    */
     //Competition Robot Settings
     public static final int kLeftDriveId = 0;
     public static final int kRightDriveId = 1;
