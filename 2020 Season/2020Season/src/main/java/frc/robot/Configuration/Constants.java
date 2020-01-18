b939e80b1b8f322b93e54ae886ab5ce3d4b0c885
Colin Ross <cross1@ltu.edu>
2020-01-17 20:30:15 -0500

    Merge Pull Request Test-Robot-Drive
    * Implemented the 4 spark MAX drive.
    
    * Added ramp rates and smart current limiting.

--- 2020 Season/2020Season/src/main/java/frc/robot/Configuration/Constants.java
+++ 2020 Season/2020Season/src/main/java/frc/robot/Configuration/Constants.java
@@ -3,18 +3,27 @@ package frc.robot.Configuration;
 public final class Constants {
 
     //Test Robot Settings
-    /*
-    public static final int kLeftDriveId = 0;
-    public static final int kRightDriveId = 1;
+    
+    //CAN Device IDs
+    public static final int kLeftFrontDriveId = 0;
+    public static final int kLeftRearDriveId = 1;
+    public static final int kRightFrontDriveId = 2;
+    public static final int kRightRearDriveId = 3;
 
+    //Usb Controllers
     public static final int kDriverUsbSlot = 0;
     public static final int kOperatorUsbSlot = 1;
-    */
+
+    //Spark MAX Configuration
+    public static final double kDriveRampRateSeconds = 0.1;
+    public static final int kDriveSmartCurrentLimitAmps = 40;
+
+    /*
     //Competition Robot Settings
     public static final int kLeftDriveId = 0;
     public static final int kRightDriveId = 1;
 
     public static final int kDriverUsbSlot = 0;
     public static final int kOperatorUsbSlot = 1;
-
+    */
 }
